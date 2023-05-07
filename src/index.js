d655ee0fe28c7ddbc1923dc67cf470a33c2fe4fa
Mary <maryandreeva100@gmail.com>
2023-05-07 18:04:28 +0300

    location and footer

--- src/index.js
+++ src/index.js
@@ -21,24 +21,33 @@ slidersInit('.career__slider', {
   }, 
   breakpoints: {
     768: {
-      slidesPerView: 3,
-      spaceBetween: 20
+      slidesPerView: 'auto',
+      spaceBetween: 20, 
+      pagination: false,
     },
     1024: {
-      // slidesPerView: 3,
-      spaceBetween: 26
+      slidesPerView: 'auto',
+      spaceBetween: 26,
+      pagination: false,
     },
     1240: {
-      slidesPerView: 4,
-      spaceBetween: 30
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      pagination: false,
     }
   }
 })
 
 
 // переместить
-const videoBG = document.querySelector('.video-bg');
-videoBG.innerHTML = `
+const videoBGHero = document.querySelector('.video-bg-hero');
+videoBGHero.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
+
+const videoBGLocation = document.querySelector('.video-bg-location');
+videoBGLocation.innerHTML = `
   <source src="video/video.webm" type="video/webm">
   <source src="video/video.mp4" type="video/mp4">
 `;
